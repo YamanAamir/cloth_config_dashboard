b113b5b62e10a4b8a3a1fdd73da09daf7a92a3cf
Yaman Aamir <136349726+YamanAamir@users.noreply.github.com>
2026-02-20 07:18:08 +0500

    added

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
 });
 
 const apiFormdata = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'multipart/form-data',
     },
